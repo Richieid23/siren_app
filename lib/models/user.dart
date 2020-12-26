076c45eeb1efda484f4c2c7137b34f56d955e833
Richieid23 <fitrarizki23@gmail.com>
2020-12-26 10:32:20 +0700

    add models & services

---
+++ lib/models/user.dart
@@ -0,0 +1,69 @@
+part of 'models.dart';
+
+class User extends Equatable {
+  final int id;
+  final String name;
+  final String email;
+  final String address;
+  final String houseNumber;
+  final String phoneNumber;
+  final String city;
+  final String picturePath;
+  static String token;
+
+  User(
+      {this.id,
+      this.name,
+      this.email,
+      this.address,
+      this.houseNumber,
+      this.phoneNumber,
+      this.city,
+      this.picturePath});
+
+  factory User.fromJson(Map<String, dynamic> data) => User(
+        id: data['id'],
+        name: data['name'],
+        email: data['email'],
+        address: data['address'],
+        houseNumber: data['houseNumber'],
+        phoneNumber: data['phoneNumber'],
+        city: data['city'],
+        picturePath: data['profile_photo_url'],
+      );
+
+  User copyWith({
+    int id,
+    String name,
+    String email,
+    String address,
+    String houseNumber,
+    String phoneNumber,
+    String city,
+    String picturePath,
+  }) =>
+      User(
+          id: id ?? this.id,
+          name: name ?? this.name,
+          email: email ?? this.email,
+          address: address ?? this.address,
+          houseNumber: houseNumber ?? this.houseNumber,
+          phoneNumber: phoneNumber ?? this.phoneNumber,
+          city: city ?? this.city,
+          picturePath: picturePath ?? this.picturePath);
+
+  @override
+  List<Object> get props =>
+      [id, name, email, address, houseNumber, phoneNumber, city, picturePath];
+}
+
+User mockUser = User(
+    id: 1,
+    name: 'Fitra Rizki',
+    address: 'Jalan Perusahaan',
+    city: 'Malang',
+    houseNumber: '1234',
+    phoneNumber: '08123456789',
+    email: 'rizki@siren.com',
+    picturePath:
+        'https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg');
